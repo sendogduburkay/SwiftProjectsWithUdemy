0608c38cbc23bede3cae35b44ba3fad766f26570
sendogduburkay <sendogduburkay@gmail.com>
2022-08-26 01:53:36 +0300

    InstaCloneParse project has been added.

---
+++ InstaCloneParse/InstaCloneParse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaCloneParse
+//
+//  Created by Muhammed Burkay Şendoğdu on 26.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
