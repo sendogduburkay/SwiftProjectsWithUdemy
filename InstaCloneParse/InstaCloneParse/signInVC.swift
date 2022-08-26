e9950db918a4459877e3df331f1e5ca35e049107
sendogduburkay <sendogduburkay@gmail.com>
2022-08-27 01:32:32 +0300

    SignInVC objects created in storyboard. Connected with SignInVC

--- InstaCloneParse/InstaCloneParse/signInVC.swift
+++ InstaCloneParse/InstaCloneParse/signInVC.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class signInVC: UIViewController {
-
+    @IBOutlet var usernameText: UITextField!
+    
+    @IBOutlet var passwordText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func signUpButtonClicked(_ sender: Any) {
+    }
 }
 
