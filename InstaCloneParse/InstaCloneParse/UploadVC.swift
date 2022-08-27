d2eda230e1da95cc07fdc5d97af8c5d4a1acf868
sendogduburkay <sendogduburkay@gmail.com>
2022-08-27 05:08:37 +0300

    UIImagePickerController and hideKeyboard function is added.

--- InstaCloneParse/InstaCloneParse/UploadVC.swift
+++ InstaCloneParse/InstaCloneParse/UploadVC.swift
@@ -7,17 +7,46 @@
 
 import UIKit
 
-class UploadVC: UIViewController {
+class UploadVC: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var commentText: UITextField!
+    @IBOutlet var postButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let keyboardRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        self.view.addGestureRecognizer(keyboardRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(choosePhoto))
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
+        postButton.isEnabled = false
+    }
+    
+    @objc func hideKeyboard(){
+        self.view.endEditing(true)
+    }
+    
+    @objc func choosePhoto(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        present(picker,animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as? UIImage
+        imageView.image = image
+        dismiss(animated: true)
+        postButton.isEnabled = true
+        
     }
     
     @IBAction func postButtonClicked(_ sender: Any) {
+        postButton.isEnabled = false
     }
     
     /*
